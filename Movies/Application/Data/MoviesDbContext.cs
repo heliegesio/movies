db6657f260e832bb6bf3d1a75f058abb660e40ff
Heliegesio Soares Souza <heliegesio.souza@e-cartoriotcs.com.br>
2024-12-09 08:22:06 -0300

    ok

---
+++ Movies/Application/Data/MoviesDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Movies.Application.Models;
+
+namespace Movies.Application.Data
+{
+    public class MoviesDbContext : DbContext
+    {
+        public MoviesDbContext(DbContextOptions<MoviesDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Producer> Producers { get; set; }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Producer>().HasKey(m => m.Id);
+            base.OnModelCreating(builder);
+        }
+    }
+}
--- Movies/Application/Models/Producer.cs
+++ Movies/Application/Models/Producer.cs
@@ -2,7 +2,8 @@
 {
     public class Producer
     {
-        public string Name { get; set; }
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
         public int Interval { get; set; }
         public int PreviousWin { get; set; }
         public int FollowingWin { get; set; }
