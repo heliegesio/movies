2814fdfa6fe2bf077f5c5f553afdcf31d57d06de
Heliegesio Soares Souza <heliegesio.souza@e-cartoriotcs.com.br>
2024-12-09 08:50:27 -0300

    ok

---
+++ Movies/Migrations/MoviesDbContextModelSnapshot.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Movies.Application.Data;
+
+namespace Movies.Migrations
+{
+    [DbContext(typeof(MoviesDbContext))]
+    public class MoviesDbContextModelSnapshot : ModelSnapshot
+    {
+        protected override void BuildModel(ModelBuilder modelBuilder)
+        {
+
+            modelBuilder
+                .HasDefaultSchema("Business")
+                .HasAnnotation("ProductVersion", "6.0.11")
+                .HasAnnotation("Relational:MaxIdentifierLength", 63);
+
+            modelBuilder.Entity("Movies.Application.Models.Producer", b =>
+            {
+                b.Property<Guid>("Id")
+                    .HasColumnType("uuid");
+
+                b.Property<string>("Name")
+                    .HasColumnType("text");
+
+                b.Property<DateTime?>("Interval")
+                    .HasColumnType("integer");
+
+                b.Property<DateTime?>("PreviousWin")
+                    .HasColumnType("integer");
+
+                b.Property<DateTime?>("FollowingWin")
+                    .HasColumnType("integer");
+
+            });
+
+            
+        }
+    }
+}
--- Movies/Program.cs
+++ Movies/Program.cs
@@ -1,4 +1,7 @@
+using Microsoft.EntityFrameworkCore;
+using Movies.Application.Data;
 using Movies.Application.Handlers;
+using Microsoft.EntityFrameworkCore.Design;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,7 +12,10 @@ builder.Services.AddSingleton<ProducerHandler>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+var connectionString = builder.Configuration.GetConnectionString("SqliteConnectionString");
+builder.Services.AddDbContext<MoviesDbContext>(options =>
+    options.UseSqlite(connectionString)
+);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
